58ae48996503b010fe22dcd1adba6d881bf4bf76
hicham <mouradxhicham@gmail.com>
2020-04-10 20:11:20 +0100

    initial commit

---
+++ src/main/java/com/gestioncontabilite/gestionfst/GestionfstApplication.java
@@ -0,0 +1,13 @@
+package com.gestioncontabilite.gestionfst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionfstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionfstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gestioncontabilite/gestionfst/GestionfstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestioncontabilite.gestionfst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionfstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
